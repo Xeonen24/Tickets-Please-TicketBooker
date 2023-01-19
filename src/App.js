371c899ec3154f3b0fac685a8826108444fbade6
Xeonen24 <108137169+Xeonen24@users.noreply.github.com>
2023-01-19 19:46:41 +0530

    xd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import 'swiper/swiper.min.css';
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
-import React from 'react';
+import React, { createContext, useReducer ,useState,useEffect} from 'react';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup';
+import Logout from './pages/logout/logout';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Detail from './pages/detail/Detail';
+import {Loading} from './components/loading/loading';
+import { BeatLoader } from 'react-spinners'
+import  ClipLoader from 'react-spinners/ClipLoader'
+import {initialState,reduce} from './reducer/UseReducer';
 
-function App() {
+export const UserContext = createContext();
+export const loggedIN = localStorage.getItem("isLoggedIn")
+
+
+const App = () =>{
+    const [state, dispatch]= useReducer(reduce,initialState)
+    const [load,setLoading] = useState(false)
+    useEffect(() =>{
+        setLoading(true)
+        setTimeout(() =>{
+            setLoading(false)
+        },1200)
+    },[])
+    
     return (
         <BrowserRouter>
-            <Route render={props => (
+            <Route render={props => ( 
                 <>
+                {
+                 load?
+                  <ClipLoader className="spinner" color={'#D00218'} loading={load} size={200}/>
+                     :
+                     <UserContext.Provider value={{state, dispatch}}>
                     <Header {...props}/>
                     <Switch>
                     <Route path="/login" component={Login} />
                     <Route path="/signup" component={Signup} />
+                    <Route path="/logout" component={Logout} />
                             <Route path='/:category/search/:keyword'component={Catalog}/>
                              <Route path='/:category/:id' component={Detail}/>
                             <Route path='/:category' component={Catalog}/>
                             <Route path='/' exact component={Home}/>
                     </Switch>
-                <Footer/>
+                    <Footer/>
+                </UserContext.Provider>
+                 }
                 </>
             )}/>
         </BrowserRouter>
