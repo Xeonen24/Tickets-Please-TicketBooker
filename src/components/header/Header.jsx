896cf33f59327e603b5c248185a90d27e97adf4f
Xeonen24 <108137169+Xeonen24@users.noreply.github.com>
2023-01-19 18:41:24 +0530

    fully working login

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,45 @@
-import React, { useRef, useEffect } from 'react';
-import { Link,useHistory } from 'react-router-dom';
-import axios from "axios";
-import { toast} from 'react-toastify';
+import React, { useRef, useEffect,useContext } from 'react';
+import { Link } from 'react-router-dom';
 import './header.scss';
 import logo from '../../assets/tmovie.png';
+import { UserContext } from "../../App";
+import {loggedIN} from '../../App'
 
 const Header = () => {
-    let history = useHistory();
+    const {state,dispatch} = useContext(UserContext)
     const headerRef = useRef(null);
+
+    const NavMenu = () =>{
+        if(state,loggedIN){
+            return(
+                <>
+                <li>
+                 <Link to="/movie" style={{textDecoration: "none"}}>Movies</Link>
+                </li>
+                <li>
+                 <Link to="/tv" style={{textDecoration: "none"}}>TV Series</Link>
+                </li>
+                <li>
+                <Link to="/logout">Logout</Link>
+                </li>
+                </>
+            )
+        }else{
+            return (
+                <>
+                <li>
+                 <Link to="/movie" style={{textDecoration: "none"}}>Movies</Link>
+                </li>
+                <li>
+                 <Link to="/tv" style={{textDecoration: "none"}}>TV Series</Link>
+                </li>
+                <li>
+                 <Link to="/login">Sign in/up</Link>
+                </li>
+                </>
+            )
+        }
+    }
     useEffect(() => {
         const shrinkHeader = () => {
             if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -21,18 +53,6 @@ const Header = () => {
             window.removeEventListener('scroll', shrinkHeader);
         };
     }, []);
-    const logOut = () =>{
-        axios.get('http://localhost:5000/api/logout')
-        .then(result =>{
-            toast.success('Log out successfully');
-            localStorage.removeItem('token');
-            history.push('/');
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-
-    }
 
     return (
         <div ref={headerRef} className="header">
@@ -42,18 +62,7 @@ const Header = () => {
                     <Link to="/">CinemaSeatsDirect</Link>
                 </div>
                 <ul className="header__nav">
-                    <li>
-                        <Link to="/movie" style={{textDecoration: "none"}}>Movies</Link>
-                    </li>
-                    <li>
-                        <Link to="/tv" style={{textDecoration: "none"}}>TV Series</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Sign in/up</Link>
-                    </li>
-                    <li>
-                        <Link to="" onClick={logOut} >Logout</Link>
-                    </li>
+                    <NavMenu />
                 </ul>
             </div>
         </div>
